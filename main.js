9df7275004ff015f607a6c52c0ae9e71aa53ad42
chandanaavirneni <146082946+chandanaavirneni@users.noreply.github.com>
2023-09-28 17:42:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function stringNumcon(){
+
+let name="chandana";
+let phonenumber=95332393;
+a=(name+" : "+phonenumber);
+document.getElementById("l1").textContent=a;
+}
+
+function Boolean(){
+const isTodayThursdat=true;
+    b=isTodayThursdat;
+    document.getElementById("l2").textContent=b;
+}
